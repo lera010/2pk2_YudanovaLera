3fd86ed6c9c059360c6d277a1e56dd32fa31f998
Лера <lerayudanova05@gmail.com>
2023-09-15 22:56:46 +0500

    добавлена 2 практика

--- PZ_02/Program.cs
+++ PZ_02/Program.cs
@@ -5,28 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Введите действительное число:");
-            double a = double.Parse(Console.ReadLine());
+            double a = double.Parse(Console.ReadLine()); //объявление переменной
             Console.WriteLine("Введите целое число:");
-            double c = double.Parse(Console.ReadLine());
-            double pi = Math.PI;
-            double x, y, z;
-            if (a > pi)
+            double c = double.Parse(Console.ReadLine()); //объявление переменной
+            double pi = Math.PI; //переменная со значением числа пи
+            double x, y, z; //указание типа переменных
+            if (a > pi) 
 
-                x = Math.Cos(a + 2 * c);
+                x = Math.Cos(a + 2 * c); //вычисление, если введенное пользователем число больше пи
 
             else
 
-                x = c + Math.Sqrt(a * (c * c) - 2 * (a + pi));
+                x = c + Math.Sqrt(a * (c * c) - 2 * (a + pi)); //вычисление, если введенное пользователем число меньше или равно пи
 
             if (x <= 0)
 
-                y = Math.Log(a + c - x);
+                y = Math.Log(a + c - x);//вычисление, если найденная переменная x меньше или равна нулю
 
             else
 
-                y = Math.Sin(2 * a * x) + Math.Sin(a + x);
+                y = Math.Sin(2 * a * x) + Math.Sin(a + x);//вычисление, если найденная переменная x больше нуля
 
-            z = (2 * x + 3 * y) * (a * a - c * c);
+            z = (2 * x + 3 * y) * (a * a - c * c); //вычисление конечного результата с найденными переменными x и y
+
             Console.WriteLine("Результат: " + z);
         }
     }
