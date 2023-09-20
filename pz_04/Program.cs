475b294569b15ddd162d752ae7a91eaa6e828d9d
Лера <lerayudanova05@gmail.com>
2023-09-20 21:49:51 +0500

    добавлена 4 практика

--- pz_04/Program.cs
+++ pz_04/Program.cs
@@ -5,44 +5,49 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Задание №1 "); //задание 1
-            int a ; 
-            for (a = -100; a <= 20; a +=3)
-             Console.WriteLine(a);
-            Console.WriteLine(  );//пустая строка
+            for (int a = -100; a <= 20; a +=3)
+             Console.WriteLine(a); // выводятся значения от -100 до 20 с шагом 3
+            Console.WriteLine(  ); //пустая строка для разграничения заданий
+
 
 
             Console.WriteLine("Задание №2 "); // задание 2
             char m = 'ш';
             for (char simvol = 'п'; simvol<=m ; simvol++)
-            Console.WriteLine(simvol);
-            Console.WriteLine(  );//пустая строка
+            Console.Write(simvol+" "); //выводится 10 букв в алфавитном порядке, начиная с "п"
+            Console.WriteLine( "\n" ); //пустая строка для разграничения заданий
+
 
 
             Console.WriteLine("Задание №3 "); // задание 3
-            int b, c;
-            for (b = 0; b < 3; b++)
-            { for (c = 0; c < 10; c++)
-                { Console.Write("#"); }
+            for (int b = 0; b < 3; b++) //выведет 3 строки с 10 символами "#"
+            { for (int c = 0; c < 10; c++) //выведет строку с 10 символами "#"
+                { Console.Write("#"); } 
               Console.WriteLine(  ); 
             }
-            Console.WriteLine(  );//пустая строка
+            Console.WriteLine(  ); //пустая строка для разграничения заданий
+
 
 
             Console.WriteLine("Задание №4 "); // задание 4
-            int s = 0;  
-            for (int f = -300; f < 200; f++)
-            { if (f % 6 == 0)
-                    Console.Write(f+" ");
-                s++;
-                 }
-            Console.WriteLine("количество кратных чисел равно: "+ s);
-            Console.WriteLine(  );//пустая строка
+            int f = 0; //ввод переменной, используемой для счетчика кратных чисел
+            for (int i = -300; i < 200; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    Console.Write(i + " "); //выводит числа кратные 6 (условие if) из диапазона чисел от -300 до 200 (цикл for)
+                    f++; //счетчик кратных чисел
+                }
+            }
+            Console.WriteLine("\nКоличество кратных чисел: " + f); //с новой строки выводит сообщение с количеством кратных чисел
+            Console.WriteLine(  ); //пустая строка для разграничения заданий
+
 
 
             Console.WriteLine("Задание №5 "); // задание 5
-            int t, e;
+            int t, e; //объявление переменных
             for (t =2, e =30; t-e <= 6;  t++, e--)
-            { Console.WriteLine(t+" "+e); }
+            { Console.WriteLine(t+" "+e); } //вывод инкрементированных и декрементированных переменных пока разница между ними не будет меньше 6
 
         }
     }
