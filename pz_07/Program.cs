222e5118bd6c4237813401bde193c03688b4205b
Лера <lerayudanova05@gmail.com>
2023-09-29 23:44:57 +0500

    добавлена 7 практика

---
+++ pz_07/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace pz_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            Console.Write("Введите число строк массива:");
+            int a =Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nВведите число столбцов массива:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int min = Int32.MaxValue;//объявление переменной обозначающей максимальное число
+            int indexmin = 0;
+            int[,] array = new int [a, b];
+            for (int i=0; i<a; i++)
+            {
+                for (int j=0; j<b; j++)
+                {   
+                    array [i,j] = rnd.Next(-10000,10000);
+                    Console.Write(array[i,j]+ "\t");
+                    int sum = 0;
+                    sum += array[i, j];
+                    
+                    if (sum < min)
+                    {
+                        min = sum;
+                        indexmin = i;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Строка с минимальной суммой элементов");
+            for (int j = 0; j < b; j++)
+            {
+                Console.Write(array[indexmin, j] + "\t");
+            }
+        }
+    }
+}
