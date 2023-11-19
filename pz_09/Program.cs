b604d1ad051ba81a7eb0ccf832353c8f32145654
Лера <lerayudanova05@gmail.com>
2023-11-19 13:25:46 +0500

    практика 9

--- pz_09/Program.cs
+++ pz_09/Program.cs
@@ -5,26 +5,28 @@
         static void Main(string[] args)
         {
             string password = Console.ReadLine();
-            int length = 0; //cсоответствие длине строки
-            int reg = 0; //наличие символов в верхнем регистре
-            int reg2 = 0; //наличие символов в нижнем регистре
-            int num = 0; // наличие цифр
-            int symbols = 0; //наличие спец символов
+            int length = 0; 
+            int reg = 0; 
+            int reg2 = 0; 
+            int num = 0; 
+            int symbols = 0; 
             for (int i = 0; i < password.Length; i++)
             {
-                if (Char.IsDigit(password[i])) num = 1;
+                if (Char.IsDigit(password[i])) //проверка на наличие цифр
+                    num = 1;
 
-                if (Char.IsUpper(password[i])) reg = 1;
+                if (Char.IsUpper(password[i])) //проверка на наличие верхнего регистра
+                    reg = 1;
 
-                if (Char.IsLower(password[i])) reg2 = 1;
+                if (Char.IsLower(password[i])) //проверка на наличие нижнего регистра
+                    reg2 = 1;
 
-                if (password[i] == '!' ||
-                    password[i] == '-' ||
-                    password[i] == '_' ||
-                    password[i] == '.') symbols = 1;
+                if (password[i] == '!' || password[i] == '-' || password[i] == '_' || password[i] == '.') //проверка на наличие символов
+                    symbols = 1;
             }
 
-            if (password.Length > 8) length = 1;
+            if (password.Length > 8) //проверка на длину пароля
+                length = 1;
 
             if (length==1 && reg==1 && reg2==1 && num==1 && symbols==1)
                 Console.WriteLine("пароль соответствует требованиям");
