34b4d3c0eda5adf1cb52fcf2bd3a9cb8f2b8040a
Лера <lerayudanova05@gmail.com>
2023-11-21 22:19:16 +0500

    практика 10

--- pz_10/Program.cs
+++ pz_10/Program.cs
@@ -6,51 +6,27 @@
         {
             Console.WriteLine("Введите текст");
             string text = Console.ReadLine();
-            
             double sum = 0;
             int c = 0;
-            
-            /*while (i < text.Length)
-            {
+ 
+            for (int i = 0; i < text.Length; i++)
+            {   
                 string num = "";
 
-                while (i < text.Length && Char.IsDigit(text[i]))
+                while (i < text.Length && Char.IsDigit(text[i])) //проверка на то является ли символ цифрой и добавление чисел в строку
                 {
                     num += text[i];
                     i++;
                 }
 
-                if (!String.IsNullOrEmpty(num))
+                if (!String.IsNullOrEmpty(num)) //если строка не пустая, числа из строки суммируются
                 {
                     sum += int.Parse(num);
-                    c++;
+                    c++; //обновление счетчика количества чисел
                 }
-                i++;
             }
-            double sr = Math.Round((sum / c), 2);
-            Console.WriteLine("Среднее арифметическое равно: " + sr);*/
-
-
-
-
-            for (int i = 0; i < text.Length; i++)
-            {   string num = "";
-                while (i < text.Length && Char.IsDigit(text[i]))
-                {
-                    num += text[i];
-                    i++;
-                }
-                if (!String.IsNullOrEmpty(num))
-                {
-                    sum += int.Parse(num);
-                    c++;
-                }
-                Console.WriteLine( num);
-            }
-            double sr = Math.Round((sum / c), 2);
+            double sr = Math.Round((sum / c), 2); //нахождение среднего арифметического и округление 
             Console.WriteLine("Среднее арифметическое равно: " + sr); 
-
-
         }
     }
 }
