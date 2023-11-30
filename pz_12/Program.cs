ce5a53cd6e710bd338b6fa95ea40c41cca8e86c2
Лера <lerayudanova05@gmail.com>
2023-11-30 19:34:07 +0500

    практика 12

---
+++ pz_12/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace pz_12
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                Console.Write("Введите количество цифр: ");
+                int length = int.Parse(Console.ReadLine());//определение длины массива
+
+                int[] nums = new int[length];
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    Console.Write("Введите цифру: ");
+                    nums[i] = int.Parse(Console.ReadLine());//заполнение массива
+                }
+                Console.WriteLine(numsDegree2(nums));//вызов метода возвращающего количество элементов
+        }
+
+            static int numsDegree2(int[] nums)//метод возвращающий количество элементов
+            {
+                int countDegree = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (isDegree(nums[i]))//вызов доп метода
+                    {
+                        countDegree++;//увеличение счетчика
+                    }
+                }
+                return countDegree;
+            }
+
+            
+            static bool isDegree(int num)//дополнительный метод, принимающий число и возвращающий true/false в зависимости от результата.
+            {
+                while (num != 1)//цикл проверяющий является ли число степенью двойки
+                {
+                    if (num % 2 != 0)
+                    {
+                        return false;
+                    }
+                    num /= 2;
+                }
+                return true;
+        }
+        }
+    }
