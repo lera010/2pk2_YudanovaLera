3dff0f56db189809967909dd333543c71993003a
Лера <lerayudanova05@gmail.com>
2024-02-21 16:03:25 +0500

    практика 18

---
+++ pz_18/Credit.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_18
+{
+    enum Status { high, average, low }
+    internal class Credit
+    {
+        public string Name { get; set; }
+        public Status Status { get; set; }
+        public decimal Sum { get; set; }
+
+        public static int clients = 0;
+        public static decimal allcredits = 0;
+        public Credit(string name, Status status, int sum)
+        {
+            Name = name;
+            Status = status;
+            Sum = sum;
+
+            if (string.IsNullOrEmpty(name))
+                Console.WriteLine("Поле ФИО не может быть пустым");
+
+            if (sum < 100000 || sum > 5000000)
+                Console.WriteLine("Сумма кредита не может быть меньше 100000 и больше 5000000");
+
+            clients++;
+            allcredits += sum;
+        }
+        public void PrintCreditInfo()
+        {
+            Console.WriteLine($"ФИО: {Name}");
+            if (Status == Status.high)//проверка на статус кредитной истории
+                Console.WriteLine($"Статус кредитной истории: высокий \n Процент по кредиту: 10");
+            else if (Status == Status.average)
+                Console.WriteLine($"Статус кредитной истории: средний \n Процент по кредиту: 12");
+            else
+                Console.WriteLine("Процент по кредиту: отсутствует");
+            Console.WriteLine($"Сумма кредита: {Sum}");
+        }
+        public void GetCredit()//метод выдачи кредита
+        {
+            if (allcredits < 5000000)//проверка на остаток средств в банке
+            {
+                if (Status == Status.high)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Кредит одобрен");
+                }
+                else if (Status == Status.average)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Кредит одобрен");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Кредит отказан");
+                    if (clients != 0)
+                        clients--;
+                    allcredits -= Sum;
+                }
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Кредит отказан(недостаточно средств в банке)");
+                if (clients != 0)
+                    clients--;
+                allcredits -= Sum;
+            }
+        }
+        public static void PrintBankInfo()
+        {
+            Console.WriteLine($"Сумма всех кредитов в банке:{allcredits}");
+            Console.WriteLine($"Количество выданных кредитов:{clients}");
+            Console.WriteLine($"Остаток средств банка:{5000000 - allcredits}");
+            Console.WriteLine("-------------------------------------------------");
+        }
+    }
+}
---
+++ pz_18/Program.cs
@@ -0,0 +1,60 @@
+﻿using pz_18;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Credit credit1 = new Credit("Ivan", Status.high, 105000);
+            credit1.GetCredit();
+            credit1.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit2 = new Credit("Anna", Status.low, 105000);
+            credit2.GetCredit();
+            credit2.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit3 = new Credit("Yulia", Status.high, 5200000);
+            credit3.GetCredit();
+            credit3.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit4 = new Credit("Anastasia", Status.high, 4500000);
+            credit4.GetCredit();
+            credit4.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit5 = new Credit("Vladislav", Status.average, 105000);
+            credit5.GetCredit();
+            credit5.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit6 = new Credit("Zhania", Status.high, 115000);
+            credit6.GetCredit();
+            credit6.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit7 = new Credit("Valeria", Status.low, 99000);
+            credit7.GetCredit();
+            credit7.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit8 = new Credit("Sofia", Status.high, 308000);
+            credit8.GetCredit();
+            credit8.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit9 = new Credit("Sergei", Status.average, 215000);
+            credit9.GetCredit();
+            credit9.PrintCreditInfo();
+            Credit.PrintBankInfo();
+
+            Credit credit10 = new Credit("Ekaterina", Status.low, 105000);
+            credit10.GetCredit();
+            credit10.PrintCreditInfo();
+            Credit.PrintBankInfo();
+        }
+    }
+}
